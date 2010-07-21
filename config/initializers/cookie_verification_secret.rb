5e2da877bba81c564e4939661f14cffae2fe003d
James Harrison <james@balvedastudios.com>
2010-07-21 03:07:56 +0000

    Initial commit with appropriate gitignore, rspec scaffold, database config sample

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'cf5c767bdb0c2ef55041678be2d824fbae155c8e8f551a487341fd4c13782c556d8d137092154035c186c5f5f50d4de9b48fe967fd0753dbb6043e69e0f901a8';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_nebula_session',
+  :secret      => 'ff0d6fec7df84d8289ae90ac7c5a929477d28ab093a6a481e227ea5132a66dbfd112952b0269300c93806a9baf4dbc04c59c630a4ff8b0ff150a3e94c005b731'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
