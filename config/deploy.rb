61900107cb18cd64a592de167b7b9a0819729c5d
Dominik Sander <git@dsander.de>
2010-09-25 22:55:29 +0000

    deployment the second

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :run_method, :run
 
 set :repository,  "git://github.com/excellence/nebula.git"
 set :scm, :git
-set :branch, "master"
+set :branch, "develop"
 set :deploy_via, :remote_cache
 
 
@@ -40,12 +40,11 @@ end
 namespace :bundler do
   task :create_symlink, :roles => :app do
     shared_dir = File.join(shared_path, 'vendor_bundle')
-    release_dir = File.join(current_release, 'bundle')
+    release_dir = File.join(current_release, 'vendor/bundle')
     run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
   end
  
   task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
     run "cd #{release_path} && bundle install --deployment --without test"
   end
 end
